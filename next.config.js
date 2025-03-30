50d4a0317c2e148d528558870a7a0c62a6dd73de
Kleant Zogu <kleantizogu@gmail.com>
2025-03-30 20:02:11 +0200

    Update next.config.js for standalone output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = process.env.ANALYZE === 'true' 
+  ? require('@next/bundle-analyzer')({})
+  : (config) => config
+
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  // Disable static optimization to prevent hydration errors with useSearchParams()
+  reactStrictMode: false,
+  swcMinify: true
+}
 
 module.exports = withBundleAnalyzer(nextConfig)
