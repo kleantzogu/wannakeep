7e560e1e3fe9f090e92fe71553bf69111f16bfd2
Kleant Zogu <kleantizogu@gmail.com>
2025-03-30 20:01:54 +0200

    Add config.js file to disable static rendering

---
+++ src/app/config.js
@@ -0,0 +1,6 @@
+// Disable static generation for the entire app
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+export const runtime = 'nodejs' 
